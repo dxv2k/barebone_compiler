2ebb136a07d5cc126ba6f8234dd30e7d13de3e08
knightV <9569076+dxv2k@users.noreply.github.com>
2020-06-10 02:59:34 +0700

    issue with grammar rules, need to check again'

--- parser.py
+++ parser.py
@@ -1,62 +1,130 @@
-from ply import lex,yacc 
+from ply import lex, yacc
 print('Import lex and yacc successfully')
 
-tokens = [ 
-   # Variable (identifier)
-   'identifier',
-   
-   # Basic commands 
-   'incr', 
-   'decr', 
-   'init', 
-   'clear',
-   
-   # while loop command
 
-   # copy command
+# List of tokens 
+tokens = [
+    "identifier",
+    "number",
+    "plus",
+    "minus",
+    "mult",
+    "div"
 ]
 
-for idx,keyword in enumerate(tokens): 
-   tokens[idx] = keyword.upper()
-
-for keyword in tokens: 
-   print(keyword)
-
-
-# Set of simple regular expression rules 
-# literals = [';'] # used for end of line
-t_ignore_COMMENT = r'\#.*'
-t_ignore = r' \t'
+# Tokens with regex rules
+t_ignore = r" \t"
 t_identifier = r"^[a-zA-Z]+$"
 t_number = r"[+-]?(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?"
-
-# def t_incr(): 
-#    r'\incr'
-
-# def t_decr(): 
-#    r'\decr'
-
-# def t_init(): 
-#    r'\init'
-
-# def t_clear(): 
-#    r'\clear'
-
-def t_NUMBER(t):
-    r'\d+'
-    t.value = int(t.value)
-    return t
-
-# Define a rule so we can track line numbers
-def t_newline(t):
-    r'\n+'
-    t.lexer.lineno += len(t.value)
+t_plus = r"\+"
+t_minus = r"-"
+t_mult = r"\*"
+t_div = r"/"
+# TODO: ADD t_ignore_COMMENT
 
 
-# Error handling rule
-def t_error(t):
-    print("Illegal character '%s'" % t.value[0])
-    t.lexer.skip(1)
+#-------------------------------PARSER---------------------------------- 
 
-# Build the lexer
-lexer = lex.lex()
+# Parser rules 
+# expr : expr '+' term | expr '-' term | term
+# term : term '*' factor | term '/' factor | factor
+# factor : '(' expr ')' | identifier | number
+
+
+# TODO: add p_list_stmt(p)
+
+def p_stmt(p):
+   """
+      stmt : expr
+   """
+   p[0] = ("stmt", p[1])
+
+def p_expr(p):
+   """
+      expr : expr plus term 
+           | expr minus term 
+           | term
+   """
+   p[0] = ("expr", p[1], p[2]) # Problem here <<<
+
+def p_term(p):
+   """
+      term : term mult factor 
+           | term div factor 
+           | factor
+   """
+
+def p_factor(p):
+   """
+      factor : '(' expr ')' 
+             | identifier 
+             | number
+   """
+
+
+if __name__ == "__main__":
+   lex.lex()
+   yacc.yacc()
+   data = "32 + 10"
+   result = yacc.parse(data)
+   print(result)
+
+#TODO: Build AST
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
