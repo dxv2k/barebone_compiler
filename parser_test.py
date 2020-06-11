d4cbe58993e5196441ec348577c7baed7a05665b
KnightV <9569076+dxv2k@users.noreply.github.com>
2020-06-11 19:47:22 +0700

    working on grammar rules

--- parser_test.py
+++ parser_test.py
@@ -13,8 +13,13 @@ tokens = [
 ]
 
 #-------------------REGEX-RULES------------------------------
+'''
+    Identifier regex rules can't catch illegal variable name
+    like \aszfas 
+    It consider its as tokens 
+''' 
 def t_ID(t):
-    r'[a-zA-Z_][a-zA-Z_0-9]*'
+    r'[a-zA-Z_][a-zA-Z_0-9]*' #TODO: check regex var name rules
     return t
 
 def t_NUMBER(t): 
@@ -42,46 +47,89 @@ def t_error(t):
     t.lexer.skip(1)
 
 #---------------------GRAMMAR-RULES------------------------------
+# stmt : expr 
 # expr : expr PLUS term
 #      | expr MINUS term 
 #      | term 
-# term : temr MULTI factor
-#      | temr DIV factor 
+# term : term MULT factor
+#      | term DIV factor 
 #      | factor
 # factor : NUMBER
 #        | ( expr )            
+# TODO: consider to add identifier rule for factor?  
 
 
 
+#TODO: Add list_stmt 
+#TODO: Add stmt 
 
+#def p_list_stmt(p): 
+#    ''' 
+#        list_stmt : stmt
+#    ''' 
+
+def p_stmt(p): 
+    '''
+        stmt : expr
+    ''' 
+    p[0] = ('stmt',p[1])
+
+def p_expr(p): 
+    '''
+        expr : expr PLUS term
+             | expr MINUS term 
+             | term 
+    ''' 
+    if (len(p) == 2): 
+        p[0] = p[1] 
+    p[0] = ("expr",p[0],p[1])
+
+def p_term(p): 
+    ''' 
+        term : term MULT factor
+             | term DIV factor 
+             | factor
+    ''' 
+
+def p_factor(p):  
+    ''' 
+        factor : NUMBER
+               | expr             
+    ''' 
+
+## Testing lex
+#data = ''' 
+#32 + 10
+#12 - 4
+#12 * 8
+#X
+#( )
+#( X )
+#X124
+#_123abas
+#/asba
+#'''
 
 # Build lex 
-lexer = lex.lex()
+# lexer = lex.lex()
 
 
-# Testing lex
-data = ''' 
-32 + 10
-12 - 4
-12 * 8
-X
-( )
-( X )
-X124
-_123abas
-/asba
-'''
-
-lexer.input(data)
+# lexer.input(data)
 
 
 # Tokenize
-while True:
-    tok = lexer.token()
-    if not tok: 
-        break      # No more input
-    print(tok)
+#while True:
+#    tok = lexer.token()
+#    if not tok: 
+#        break      # No more input
+#    print(tok)
  
+# Testing data for parser  
+data = '32 + 10' 
+lex.lex()
+yacc.yacc() 
+result = yacc.parse(data) 
+print(result)
 
 
 
--- parsetab.py
+++ parsetab.py
@@ -6,9 +6,9 @@ _tabversion = '3.10'
 
 _lr_method = 'LALR'
 
-_lr_signature = "div identifier minus mult number plus\n      stmt : expr\n   \n      expr : expr plus term \n           | expr minus term \n           | term\n   \n      term : term mult factor \n           | term div factor \n           | factor\n   \n      factor : '(' expr ')' \n             | identifier \n             | number\n   "
+_lr_signature = 'DIV ID LPAREN MINUS MULT NUMBER PLUS RPAREN\n        stmt : expr\n    \n        expr : expr PLUS term\n             | expr MINUS term \n             | term \n     \n        term : term MULT factor\n             | term DIV factor \n             | factor\n     \n        factor : NUMBER\n               | expr             \n    '
     
-_lr_action_items = {'(':([0,5,8,9,10,11,],[5,5,5,5,5,5,]),'identifier':([0,5,8,9,10,11,],[6,6,6,6,6,6,]),'number':([0,5,8,9,10,11,],[7,7,7,7,7,7,]),'$end':([1,2,3,4,6,7,13,14,15,16,17,],[0,-1,-4,-7,-9,-10,-2,-3,-5,-6,-8,]),'plus':([2,3,4,6,7,12,13,14,15,16,17,],[8,-4,-7,-9,-10,8,-2,-3,-5,-6,-8,]),'minus':([2,3,4,6,7,12,13,14,15,16,17,],[9,-4,-7,-9,-10,9,-2,-3,-5,-6,-8,]),')':([3,4,6,7,12,13,14,15,16,17,],[-4,-7,-9,-10,17,-2,-3,-5,-6,-8,]),'mult':([3,4,6,7,13,14,15,16,17,],[10,-7,-9,-10,10,10,-5,-6,-8,]),'div':([3,4,6,7,13,14,15,16,17,],[11,-7,-9,-10,11,11,-5,-6,-8,]),}
+_lr_action_items = {'NUMBER':([0,6,7,8,9,],[5,5,5,5,5,]),'$end':([1,2,3,4,5,10,11,12,13,14,],[0,-1,-4,-7,-8,-9,-2,-3,-5,-6,]),'PLUS':([2,3,4,5,10,11,12,13,14,],[6,-4,-7,-8,6,-2,-3,-5,-6,]),'MINUS':([2,3,4,5,10,11,12,13,14,],[7,-4,-7,-8,7,-2,-3,-5,-6,]),'MULT':([2,3,4,5,10,11,12,13,14,],[-9,8,-7,-8,-9,8,8,-5,-6,]),'DIV':([2,3,4,5,10,11,12,13,14,],[-9,9,-7,-8,-9,9,9,-5,-6,]),}
 
 _lr_action = {}
 for _k, _v in _lr_action_items.items():
@@ -17,7 +17,7 @@ for _k, _v in _lr_action_items.items():
       _lr_action[_x][_k] = _y
 del _lr_action_items
 
-_lr_goto_items = {'stmt':([0,],[1,]),'expr':([0,5,],[2,12,]),'term':([0,5,8,9,],[3,3,13,14,]),'factor':([0,5,8,9,10,11,],[4,4,4,4,15,16,]),}
+_lr_goto_items = {'stmt':([0,],[1,]),'expr':([0,6,7,8,9,],[2,10,10,10,10,]),'term':([0,6,7,8,9,],[3,11,12,3,3,]),'factor':([0,6,7,8,9,],[4,4,4,13,14,]),}
 
 _lr_goto = {}
 for _k, _v in _lr_goto_items.items():
@@ -27,14 +27,13 @@ for _k, _v in _lr_goto_items.items():
 del _lr_goto_items
 _lr_productions = [
   ("S' -> stmt","S'",1,None,None,None),
-  ('stmt -> expr','stmt',1,'p_stmt','parser.py',34),
-  ('expr -> expr plus term','expr',3,'p_expr','parser.py',40),
-  ('expr -> expr minus term','expr',3,'p_expr','parser.py',41),
-  ('expr -> term','expr',1,'p_expr','parser.py',42),
-  ('term -> term mult factor','term',3,'p_term','parser.py',48),
-  ('term -> term div factor','term',3,'p_term','parser.py',49),
-  ('term -> factor','term',1,'p_term','parser.py',50),
-  ('factor -> ( expr )','factor',3,'p_factor','parser.py',55),
-  ('factor -> identifier','factor',1,'p_factor','parser.py',56),
-  ('factor -> number','factor',1,'p_factor','parser.py',57),
+  ('stmt -> expr','stmt',1,'p_stmt','parser_test.py',72),
+  ('expr -> expr PLUS term','expr',3,'p_expr','parser_test.py',78),
+  ('expr -> expr MINUS term','expr',3,'p_expr','parser_test.py',79),
+  ('expr -> term','expr',1,'p_expr','parser_test.py',80),
+  ('term -> term MULT factor','term',3,'p_term','parser_test.py',88),
+  ('term -> term DIV factor','term',3,'p_term','parser_test.py',89),
+  ('term -> factor','term',1,'p_term','parser_test.py',90),
+  ('factor -> NUMBER','factor',1,'p_factor','parser_test.py',95),
+  ('factor -> expr','factor',1,'p_factor','parser_test.py',96),
 ]
