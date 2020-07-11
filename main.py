38cc9364868c044ee3a9eb5564d180435c795b2d
knightV <9569076+dxv2k@users.noreply.github.com>
2020-07-11 16:37:45 +0700

    designing logic of parser

--- main.py
+++ main.py
@@ -1,57 +1,46 @@
-from anytree import Node 
-from enum import Enum
+# from anytree import Node 
+# from enum import Enum
 
-clear_stmt = 'clear' 
-copy_stmt = 'copy' 
-decr_stmt = 'decr'
-incr_stmt = 'incr'
-init_stmt = 'init'
-while_stmt = 'while'
-
-
-class Token(object): 
-    def __init__(self,type,value):
-        self.type = type
-        self.value = value        
-
-# class Interpreter(obecjt): 
+# clear_stmt = 'clear' 
+# copy_stmt = 'copy' 
+# decr_stmt = 'decr'
+# incr_stmt = 'incr'
+# init_stmt = 'init'
+# while_stmt = 'while'
 
 
+# class Token(object): 
+#     def __init__(self,type,value):
+#         self.type = type
+#         self.value = value        
 
+# filename = 'factorial.bb.txt'
 
-
-
-
-filename = 'factorial.bb.txt'
-
-with open(filename) as f: 
-    program = f.readlines()
+# with open(filename) as f: 
+#     program = f.readlines()
     
 
-
-
-
-
-currLine = None  # current line of executing program 
-                 # mainly use for error notice purpose
+# currLine = None  # current line of executing program 
+#                  # mainly use for error notice purpose
 
     
+class var(): 
+    def __init__(self,name,val=0):
+        self.name = name 
+        self.val = val 
+    def getValue(self):
+        return self.val 
+    def getName(self): 
+        return self.name
 
+list_var = []
+list_var.append(var('a',4))
 
+str = 'a'
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+for var in list_var:
+    if var.getName() == str: 
+        print(True)
 
 
 
--- test_enum.py
+++ test_enum.py
@@ -12,7 +12,7 @@ class stmt_type(Enum):
     copy_stmt = auto() 
     # add_clear_stmt = auto() # TODO: This one not sure 
 
-class stmt(self): 
+class stmt(): 
     u'''
     '''
     def __init__(self,
@@ -26,13 +26,22 @@ class stmt(self):
                              # after while_loop() completed
     ): 
         self.line = current_line
-        self.type = type 
+        self.stmt_t = type 
         self.var = var
-    # TODO: add try_catch/raise exception 
-    # def return_line(self): 
-    #     return self.line
+        self.stmt_list = stmt_list
 
-class var(self): 
+    # TODO: add try_catch/raise exception 
+    def getLine(self): 
+        return self.line
+    def getType(self): 
+        return self.stmt_t
+    def getVar(self): 
+        return self.var
+    def get_stmt_list(self): 
+        return stmt_list
+
+
+class var(): 
     u''' 
     '''
     def __init__(self,
@@ -41,18 +50,53 @@ class var(self):
     ): 
         self.name = name 
         self.value = value 
+        # self.init = 1
         # TODO: add try_catch/raise exception 
 
-    def set_var(self,new_value): 
+    def setValue(self,new_value): 
         self.value = new_value 
 
-    def return_name(self): 
+    def getName(self): 
         return self.name
 
-    def return_val(self): 
+    def getValue(self): 
         return self.value
+
+
+# List for whole program
+list_var_prog = [] # Contains list of variable that used in program 
+list_stmt_prog = [] # Contains list of statement that used in program
+
+def add_stmt_to_list(list, stmt): 
+    u''' 
+        list : expected to receive list object 
+        stmt : expected to receive stmt from stmt() class 
+    ''' 
+    list.append(stmt)
+
+def set_var(var_name,var_value=0): 
+    list_var_prog.append(var(var_name,var_value)) 
 
 def find_var(var_name): 
     u''' 
         find_var() expected to receive variable from var() class
     '''
+    for var in list_var_prog: 
+        if var.getName() == var_name: 
+            return var
+    # If var is not initialed yet then find_var will init
+    # by itself 
+    set_var(var_name)
+
+def execute_stmt(stmt): 
+    return 0 
+
+def execute_stmt_list(stmt_list): 
+    return 0 
+
+
+
+
+
+
+
